cbae48e4750e2c4f5b2e79a8c41b0ef3a4c37f8a
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-17 16:10:01 +0900

    fix view #7

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,20 @@
 class HomesController < ApplicationController
+
   def index
+    @meals = Meal.all
+    # @q = Meal.all.ransack(params[:q])
+
+    # binding.pry
+    # @meals = @q.result.page(params[:page])
   end
 
   def show
+
+    if user_signed_in?
+      @meal = Meal.find(params[:id])
+      @favorite = current_user.favorites.find_by(meal_id: @meal.id)
+    end
+
   end
+  
 end
