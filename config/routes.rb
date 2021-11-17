862bd17827627cd6156393062e02a83569bcb316
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-17 20:00:14 +0900

    add users_controller #8

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
-  }  
+  }
+
+  resources :users
   
   patch 'meals', to: 'meals#read_changes'
 
