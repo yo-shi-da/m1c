c519b0ae809e9e792b18a62640b2985c0bf807b2
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-17 13:02:13 +0900

    fix controller #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :my_group
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :admin])
+  end
+
+  # 参加しているグループを取得
+  def my_group
+    if GroupUser.find_by(user_id: current_user.id).present?
+      @current_user_middle = GroupUser.find_by(user_id: current_user.id)
+      @current_user_mygroup = @current_user_middle.group
+    end
+  end
+
 end
