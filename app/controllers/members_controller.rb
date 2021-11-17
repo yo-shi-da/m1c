8da54853936882b6ccca646979dd68a97a6ba0e4
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-17 16:34:48 +0900

    add members_controller #8

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,13 @@
 class MembersController < ApplicationController
+
+  def create
+    current_user.memberses.create(group_id: params[:group_id])
+    redirect_to groups_path, notice: "参加しました"
+  end
+
+  def destroy
+    current_user.memberses.find_by(id: params[:id]).destroy
+    redirect_to groups_path, notice: "参加解除しました"
+  end
+
 end
