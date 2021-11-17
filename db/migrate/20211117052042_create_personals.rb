01798b8b95c246f3495ecd16d70a697e6f95ae4b
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-17 14:25:34 +0900

    add association  #4

--- db/migrate/20211117052042_create_personals.rb
+++ db/migrate/20211117052042_create_personals.rb
@@ -1,15 +1,15 @@
 class CreatePersonals < ActiveRecord::Migration[6.0]
   def change
     create_table :personals do |t|
-      t.date :birth_date
-      t.float :height
-      t.float :body_weight
-      t.boolena :gender
-      t.float :bmi
-      t.float :waist_circumference
-      t.float :hba_onec
-      t.text :underlying_disease
-      t.float :intake_per_oneday
+      t.date :birth_date, null: false
+      t.float :height, null: false
+      t.float :body_weight, null: false
+      t.boolean :gender, null: false
+      t.float :bmi, null: false
+      t.float :waist_circumference, null: false
+      t.float :hba_onec, null: false
+      t.text :underlying_disease, null: false
+      t.float :intake_per_oneday, null: false
       t.text :remarks
       t.references :user, null: false, foreign_key: true
 
