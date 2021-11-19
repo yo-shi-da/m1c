6289c6a4e58f346dc7b159d87927367541b8d4b0
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-19 15:03:38 +0900

    fix users_controller #35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
   
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(post_params)
+      redirect_to @user, notice: '更新しました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user.users.find(params[:id])
+  end
+
+  def user_params
+  end
+
 end
