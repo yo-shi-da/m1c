629a57af613039070cf565f4083e433da0d594cf
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-21 20:22:12 +0900

    add csv_rspec  #38

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -23,10 +23,28 @@ describe 'Post_CRテスト', type: :system do
 	# 	end
 	# end
 
-	# # OK
-	describe 'CSV' do
-		context 'CSVボタンを押したとき' do			
-			it 'CSVを出力できること' do
+	# # # OK
+	# describe 'CSV' do
+	# 	context 'CSV表示画面に移動したとき' do			
+	# 		it 'CSVが表示されていること' do
+  #       meal = FactoryBot.create(:meal)
+  #       group = FactoryBot.create(:group, name: "test_group", owner_id: meal.user_id)
+  #       FactoryBot.create(:member, user_id: meal.user_id, group_id: group.id)
+  #       visit user_session_path
+  #       fill_in :user_email, with: 'test@example.com'
+  #       fill_in :user_password, with: 'password'
+  #       click_button 'ログイン'
+  #       # click_link 'CSVエクスポート'
+	# 			expect(page).to have_link 'CSVエクスポート'
+	# 			# expect(response.headers["Cache-Control"]).to include "private"
+	# 		end
+	# 	end
+	# end
+
+		# # OK
+	describe '既読機能' do
+		context '既読ボタンを押したとき' do			
+			it '未読が既読に変わっていること' do
         meal = FactoryBot.create(:meal)
         group = FactoryBot.create(:group, name: "test_group", owner_id: meal.user_id)
         FactoryBot.create(:member, user_id: meal.user_id, group_id: group.id)
@@ -34,12 +52,9 @@ describe 'Post_CRテスト', type: :system do
         fill_in :user_email, with: 'test@example.com'
         fill_in :user_password, with: 'password'
         click_button 'ログイン'
-        click_link 'CSVエクスポート'
-				fill_in :post_owner_comment, with: 'test_post'
-				fill_in :post_owner_sugar_amount, with: '10'
-				fill_in :post_difference, with: '10'
-				click_button '登録する'
-				expect(page).to have_content '投稿しました。'
+        # click_link 'CSVエクスポート'
+				expect(page).to have_link 'CSVエクスポート'
+				# expect(response.headers["Cache-Control"]).to include "private"
 			end
 		end
 	end
