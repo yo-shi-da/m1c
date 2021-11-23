8e0c9160aa419ac868e22a622f20b9e93d8dc607
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-23 18:26:45 +0900

    fix 500error #43

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  validates :owner_comment, presence: true
+  validates :owner_sugar_amount, presence: true
+  validates :difference, presence: true
   validates :owner_sugar_amount, length: { maximum: 4 }
   validates :difference, length: { maximum: 4 }
 
