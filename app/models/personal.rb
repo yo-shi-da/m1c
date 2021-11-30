26b054c8493aaaacab1fface1f527a82fb37f7aa
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-11-30 22:01:06 +0900

    fix personal_validation #60

--- app/models/personal.rb
+++ app/models/personal.rb
@@ -1,5 +1,6 @@
 class Personal < ApplicationRecord
   validates :birth_date, presence: true
+  validate :birthday_cannot_be_in_the_future
   validates :height, presence: true
   validates :body_weight, presence: true
   validates :bmi, presence: true
@@ -10,6 +11,14 @@ class Personal < ApplicationRecord
   validates :intake_per_oneday, length: { maximum: 6 }
   
   belongs_to :user
+
+  private
+
+  def birthday_cannot_be_in_the_future
+    if birth_date.future?
+      errors.add(:birth_date, "は未来の日付は入力できません。")
+    end
+  end  
 
   enum gender: {
     男: 0,
