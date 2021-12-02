2d3a489ac9991db73282bd8bcfdaea67acb34ab7
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-12-02 18:44:43 +0900

    Fix rspec #67

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -25,15 +25,40 @@ RSpec.describe 'PostSystemTest', type: :system, js: true do
 	describe '既読機能' do
 		context '既読ボタンを押したとき' do			
 			it '未読が既読に変わっていること' do
-        meal = FactoryBot.create(:meal)
-        group = FactoryBot.create(:group, name: "test_group", owner_id: meal.user_id)
-        FactoryBot.create(:member, user_id: meal.user_id, group_id: group.id)
+				user1 = FactoryBot.create(:user, name: "user1", email: "user1@example.com", password: "password", password_confirmation: "password", admin: "false")
+				user2 = FactoryBot.create(:user, name: "user2", email: "user2@example.com", password: "password", password_confirmation: "password", admin: "false")
+        meal1 = FactoryBot.create(:meal,
+          sugar_amount: '10', 
+          sugar_cube: '25', 
+          classification: '朝', 
+          image: File.open("#{Rails.root}/spec/factories/menu5.jpg"), 
+          start_time: 'Sat, 06 Nov 2021 00:00:00 JST +09:00', 
+          reading_checks: 'false', 
+          remarks: 'name', 
+          user_id: user1.id
+        ) 
+        meal2 = FactoryBot.create(:meal,
+          sugar_amount: '100', 
+          sugar_cube: '25', 
+          classification: '朝', 
+          image: File.open("#{Rails.root}/spec/factories/menu5.jpg"), 
+          start_time: 'Sat, 06 Nov 2021 00:00:00 JST +09:00', 
+          reading_checks: 'false', 
+          remarks: 'name', 
+          user_id: user2.id
+        ) 				
+        group = FactoryBot.create(:group, name: "test_group", owner_id: user1.id)
+        FactoryBot.create(:member, user_id: user1.id, group_id: group.id)
+        FactoryBot.create(:member, user_id: user2.id, group_id: group.id)
         visit user_session_path
-        fill_in :user_email, with: 'test@example.com'
+        fill_in :user_email, with: 'user1@example.com'
         fill_in :user_password, with: 'password'
         click_button 'ログイン'
+				visit member_all_path
+				click_link 'user2'
+				click_link '食事一覧へ'
+				# binding.pry
         click_button '既読にする'
-				visit meals_path
 				expect(page).to have_content '既読'
 			end
 		end
